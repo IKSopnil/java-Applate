614234a27d9e6d37538c82e44a8a3a7c66afb024
Ibrahim Khalil Sopnil <82465527+IKSopnil@users.noreply.github.com>
2021-09-28 14:03:44 +0600

    Add files via upload

---
+++ NewApplet2A.java
@@ -0,0 +1,11 @@
+import java.applet.Applet;
+
+import java.awt.Graphics;
+
+public class NewApplet2A extends Applet
+{
+    public void paint(Graphics g)
+    {
+        g.drawString("Ibrahim Khalil ",150,100);
+    }
+}
